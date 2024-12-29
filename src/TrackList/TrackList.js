9d2f12025fecec3dfda397e82fb22382429c5f72
jeffmoore <jmo342006@gmail.com>
2024-12-29 10:09:27 -0500

    updates to all sections

---
+++ src/TrackList/TrackList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TrackList = (props) => {
+    return (
+        <div>
+            {props.tracks.map((track) => {
+                return (
+                    <Track
+                        track={track}
+                        key={track.id}
+                        onAdd={props.onAdd}
+                        isRemoval={props.isRemoval}
+                        onRemove={props.onRemoval}
+                    />
+                );
+            })}
+        </div>
+    );
+};
